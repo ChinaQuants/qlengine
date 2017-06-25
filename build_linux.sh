917233f5a33af6afeb61f999f410a079e2e20129
wegamekinglc <scrappedprince.li@gmail.com>
2017-06-25 19:22:54 +0800

    update script

--- build_linux.sh
+++ build_linux.sh
@@ -4,7 +4,17 @@ cd QuantLib
 bash autogen.sh
 ./configure prefix=$PWD
 
-export CPLUS_INCLUDE_PATH=$PWD:$CPLUS_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$PWD/include:$CPLUS_INCLUDE_PATH
+export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
+
+make -j 8
+make install
+
+cd ../QuantLib-Ext
+bash autogen.sh
+./configure prefix=$PWD
+
+export CPLUS_INCLUDE_PATH=$PWD/include:$CPLUS_INCLUDE_PATH
 export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
 
 make -j 8
