2e391d84aa7a1a6969d0569dcfeb6a50fd16d722
Cheng Li <scrappedprince.li@gmail.com>
2017-11-18 13:04:47 +0800

    run quantlib unit tests

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set BOOST_ROOT=D:/dev/boost_1_64_0
-set BOOST_LIBRARYDIR=D:/dev/boost_1_64_0/lib/stage
+set BOOST_ROOT=D:/dev/boost_1_65_1
+set BOOST_LIBRARYDIR=D:/dev/boost_1_65_1/lib/stage
 set INCLUDE=%BOOST_ROOT%;D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include
 set QL_DIR=%CD%\QuantLib
 set QLEXT_DIR=%CD%\QuantLib-Ext
@@ -26,14 +26,20 @@ cd build
 
 
 if %ADDRESS_MODEL%==Win64 (
-  cmake -G "Visual Studio 14 2015 %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
+  cmake -G "Visual Studio 14 2015 %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DQL_ENABLE_PARALLEL_UNIT_TEST_RUNNER ..
 ) else (
-  cmake -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
+  cmake -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DQL_ENABLE_PARALLEL_UNIT_TEST_RUNNER ..
 )
 
 if %errorlevel% neq 0 exit /b 1
 
 msbuild Project.sln /m:%NUMBER_OF_PROCESSORS% /p:Configuration=%BUILD_TYPE% /p:Platform=%PLATFORM%
+
+if %errorlevel% neq 0 exit /b 1
+
+cd ..\bin
+
+quantlib-test-suite --log_level=message --build_info=true
 
 if %errorlevel% neq 0 exit /b 1
 
