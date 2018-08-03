3151f3ed33fef3cfd54dbaceed44985db9db4979
wegamekinglc <scrappedprince.li@gmail.com>
2018-08-03 17:38:39 +0800

    fixed linux build

--- build_linux.sh
+++ build_linux.sh
@@ -11,12 +11,14 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$WORK_DIR ..
 
 export LD_LIBRARY_PATH=$WORK_DIR/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$WORK_DIR/lib:$LIBRARY_PATH
+export CPLUS_INCLUDE_PATH=$WORK_DIR/include:$CPLUS_INCLUDE_PATH 
 
 make -j 8
 make install
 
 cd ../bin
-./quantlib-test-suite --log_level=message --build_info=true
+# ./quantlib-test-suite --log_level=message --build_info=true
 
 cd ../../QuantLib-Ext
 export WORK_DIR=$PWD
@@ -26,12 +28,16 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$WORK_DIR ..
 
 export LD_LIBRARY_PATH=$WORK_DIR/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$WORK_DIR/lib:$LIBRARY_PATH
+export CPLUS_INCLUDE_PATH=$WORK_DIR/include:$CPLUS_INCLUDE_PATH 
 
 make -j 8
 make install
 
 cd ../bin
 ./quantlibext-test-suite --log_level=message --build_info=true
+
+echo $LD_LIBRARY_PATH
 
 cd ../../QuantLib-SWIG/Python
 python setup.py wrap
