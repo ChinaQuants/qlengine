0a092719b19c578b50c59280a943051e654befe7
wegamekinglc <scrappedprince.li@gmail.com>
2019-10-21 01:25:52 +0800

    fixed linux build

--- build_linux.sh
+++ build_linux.sh
@@ -2,6 +2,7 @@
 
 export QL_DIR=$PWD/QuantLib
 export QLEXT_DIR=$PWD/QuantLib-Ext
+export NUM_CORES="$(nproc)"
 
 cd QuantLib
 export WORK_DIR=$PWD
@@ -14,7 +15,7 @@ export LD_LIBRARY_PATH=$WORK_DIR/lib:$LD_LIBRARY_PATH
 export LIBRARY_PATH=$WORK_DIR/lib:$LIBRARY_PATH
 export CPLUS_INCLUDE_PATH=$WORK_DIR/include:$CPLUS_INCLUDE_PATH 
 
-make -j 8
+make -j $NUM_CORES
 make install
 
 cd ../bin
@@ -31,15 +32,24 @@ export LD_LIBRARY_PATH=$WORK_DIR/lib:$LD_LIBRARY_PATH
 export LIBRARY_PATH=$WORK_DIR/lib:$LIBRARY_PATH
 export CPLUS_INCLUDE_PATH=$WORK_DIR/include:$CPLUS_INCLUDE_PATH 
 
-make -j 8
+make -j $NUM_CORES
 make install
 
 cd ../bin
 ./quantlibext-test-suite --log_level=message --build_info=true
 
+cd ../../examples/c++
+export WORK_DIR=$PWD
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$WORK_DIR ..
+make -j $NUM_CORES
+make install
+
 echo $LD_LIBRARY_PATH
 
-cd ../../QuantLib-SWIG/Python
+cd ../../../QuantLib-SWIG/Python
 python setup.py wrap
 python setup.py build
 python setup.py install
